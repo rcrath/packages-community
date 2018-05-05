1a40b84208639299f70706b1f6db35d3f6e65104
Bernhard Landauer <oberon@manjaro.org>
2018-05-05 23:20:38 +0200

    add linux416-rt-extramodules

---
+++ linux416-rt-extramodules/build-extramodules.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+pwd=`pwd`
+cmd='makepkg -sf --noconfirm'
+
+if [ "`sudo cat /etc/sudoers | grep pacman`" == "" ] ; then
+   echo "please add '`whoami` ALL=NOPASSWD: /usr/bin/pacman' to your /etc/sudoers file"
+   exit 1
+fi
+
+echo '  -> cleaning environment ...'
+rm -R ${pwd}/*/{src,pkg} -f
+
+echo '  -> building extramodules ...'
+cd ${pwd}/*acpi_call && $cmd
+cd ${pwd}/*bbswitch && $cmd
+cd ${pwd}/*broadcom-wl && $cmd
+cd ${pwd}/*catalyst && makepkg -df --noconfirm
+#cd ${pwd}/*ndiswrapper && $cmd
+cd ${pwd}/*nvidia && $cmd
+cd ${pwd}/*nvidia-304xx && makepkg -d --noconfirm
+cd ${pwd}/*nvidia-340xx && makepkg -d --noconfirm
+cd ${pwd}/*nvidia-390xx && makepkg -d --noconfirm
+cd ${pwd}/*nvidiabl && $cmd
+cd ${pwd}/*r8168 && $cmd
+cd ${pwd}/*rt3562sta && $cmd
+#cd ${pwd}/*spl_zfs && $cmd
+cd ${pwd}/*tp_smapi && $cmd
+cd ${pwd}/*vhba-module && $cmd
+cd ${pwd}/*virtualbox-modules && $cmd
+
+echo '  -> cleaning up ...'
+rm -R ${pwd}/*/{src,pkg} -f
+
+cd $HOME/manjaro/pkg
+echo 'signing packages'
+cd $HOME/manjaro/pkg
+signpkgs
+
+echo 'done.'
